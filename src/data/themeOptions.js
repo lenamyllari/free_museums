73968fd84d97dc791eff26f2c2e146e9468fc103
Elena Mylläri <elena.myllari@metropolia.fi>
2020-10-04 16:06:01 +0300

    selects with service and theme options added

---
+++ src/data/themeOptions.js
@@ -0,0 +1,23 @@
+
+const themeOptions = [
+    { value: "Design ja arkkitehtuuri", label: "Design ja arkkitehtuuri"},
+    { value: "Erikoiset", label: "Erikoiset"},
+    { value: "Historia", label: "Historia"},
+    { value: "Kansainvälistä", label: "Kansainvälistä"},
+    { value: "Kotimuseot ja talomuseot", label: "Kotimuseot ja talomuseot"},
+    { value: "Käsityö", label: "Käsityö"},
+    { value: "Lapsille", label: "Lapsille"},
+    { value: "Linnat ja ulkomuseot", label: "Linnat ja ulkomuseot"},
+    { value: "Luonto ja eläimet", label: "Luonto ja eläimet"},
+    { value: "Musiikki", label: "Musiikki"},
+    { value: "Nykytaide", label: "Nykytaide"},
+    { value: "Satoja vuosia sitten", label: "Satoja vuosia sitten"},
+    { value: "Sotien historia", label: "Sotien historia"},
+    { value: "Suuret suomalaiset", label: "Suuret suomalaiset"},
+    { value: "Taide", label: "Taide"},
+    { value: "Tieto ja tekniikka", label: "Tieto ja tekniikka"},
+    { value: "Valokuva", label: "Valokuva"},
+    { value: "Vanhat mestarit", label: "Vanhat mestarit"},
+    { value: "Vesillä, teillä ja ilmassa", label: "Vesillä, teillä ja ilmassa"}]
+
+export default themeOptions
