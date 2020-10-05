f1af4125a52bd1ede7e16ad04447b859b19c151d
Tiina Vanhanen <tiina.vanhanen@metropolia.fi>
2020-10-05 10:09:14 +0300

    server cleaning & update, delete started

--- src/server/server.js
+++ src/server/server.js
@@ -2,20 +2,20 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 require('dotenv').config()
-
 const cors = require('cors')
 const museumsApi = require('./museumsApi.js')
 
 app.use(cors())
 app.use(bodyParser.json())
 //app.use(express.static('build'))
-
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(cors({
     'allowedHeaders': ['Content-Type'],
     'origin': '*',
     'preflightContinue': true
 }));
+
 const PORT = process.env.PORT
 
 app.use(function(req, res, next) {
@@ -23,10 +23,6 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
-
-
-
 
 app.use('/api', museumsApi);
 
